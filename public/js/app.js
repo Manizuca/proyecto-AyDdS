3bc46be53a175187dbc77ca851b36d4ebedc270a
Matias Zuniga <matias.nicolas.zc@gmail.com>
2017-10-04 01:06:46 -0300

    Implement a complete timer.
    Add button to add 5 minutes to countdown

--- public/js/app.js
+++ public/js/app.js
@@ -17,16 +17,30 @@ app.config(['$httpProvider',function($httpProvider) {
 }]); 
 
 app.controller('mainController', ($scope, $http, $timeout, $window, $location) => {
+    default_cd = 300;
     $scope.forms = { };
     $scope.formdata = [ ];
     $scope.timerColor = {};
     $scope.timerRunning = false;
 
-    $scope.startTimer = function (deadline) {
-        $scope.$broadcast('timer-set-countdown', deadline);
+    $scope.add5mins = function () {
+        $scope.$broadcast('timer-add-seconds', 300);
+        if ($scope.timerColor.color == 'end') {
+            $scope.startTimer();
+        }
+    };
+
+    $scope.startTimer = function () {
         $scope.$broadcast('timer-start');
         $scope.timerColor = {};
         $scope.timerRunning = true;
+    };
+
+    $scope.resetTimer = function () {
+        $scope.$broadcast('timer-stop');
+        $scope.$broadcast('timer-set-countdown', default_cd);
+        $scope.timerColor = {};
+        $scope.timerRunning = false;
     };
 
     $scope.stopTimer = function () {
@@ -38,5 +52,10 @@ app.controller('mainController', ($scope, $http, $timeout, $window, $location) =
     $scope.$on('timer-stopped', function (event, data) {
         $scope.timerColor.color = 'end';
         $scope.timerRunning = false;
+    });
+
+    var unregister =  $scope.$watch('hhours', function() {
+        unregister();
+        $scope.resetTimer();
     });
 });
