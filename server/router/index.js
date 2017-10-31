1c3ca93438f9fb2366ceb91849cf9376a0208d72
cdr <cdr@iteratec.de>
2017-10-31 13:29:04 -0300

    added controllers for models

--- server/router/index.js
+++ server/router/index.js
@@ -2,40 +2,49 @@
 var mainRoutes = require('./main');
 var passportRoutes = require('./passport');
 var path = require('path');
+var models = require('../models');
+models.sequelize
+  .authenticate()
+  .then(function () {
+    console.log('Connection successful');
+  })
+  .catch(function (error) {
+    console.log("Error creating connection:", error);
+  });
 
-module.exports = function(app, passport) {
-    app.use('/', mainRoutes);
+module.exports = function (app, passport) {
+  app.use('/', mainRoutes);
 
-    passportRoutes(app, passport);
+  passportRoutes(app, passport);
 
-    //app.use('/api', apiRoutes);
+  //app.use('/api', apiRoutes);
 
-    // not found handler
-    app.use((req, res, next) => {
-      res.status(404);
-      res.sendFile(path.join(
-            __dirname, '..', '..', 'views', '404.html'));
-    });
+  // not found handler
+  app.use((req, res, next) => {
+    res.status(404);
+    res.sendFile(path.join(
+      __dirname, '..', '..', 'views', '404.html'));
+  });
 
-    // error handlers
+  // error handlers
 
-    // development error handler
-    // will print stacktrace
-    if (app.get('env') === 'development') {
-      app.use((err, req, res, next) => {
-        res.status(err.status || 500);
-        res.json({
-          message: err.message,
-          error: err
-        });
-      });
-    }
-
-    // production error handler
-    // no stacktraces leaked to user
+  // development error handler
+  // will print stacktrace
+  if (app.get('env') === 'development') {
     app.use((err, req, res, next) => {
-      var code = err.status || 500;
-      res.status(code);
-      res.render('500', {code: code, errmsg: err.message, title: "Error " + code});
+      res.status(err.status || 500);
+      res.json({
+        message: err.message,
+        error: err
+      });
     });
+  }
+
+  // production error handler
+  // no stacktraces leaked to user
+  app.use((err, req, res, next) => {
+    var code = err.status || 500;
+    res.status(code);
+    res.render('500', { code: code, errmsg: err.message, title: "Error " + code });
+  });
 }
