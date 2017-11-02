0af2dd105481b8b4e21aaabd914cf724751040c6
cdr <cdr@iteratec.de>
2017-11-02 13:07:46 -0300

    cahnged api name

--- server/api/index.js
+++ server/api/index.js
@@ -13,28 +13,28 @@ router.put('/decision', decisions.update);
 router.delete('/decision', decisions.delete);
 
 //scenario API
-router.get('/scenario', scenario.index);
+router.get('/scenarios', scenario.index);
 router.get('/scenario/:id', scenario.show);
 router.post('/scenario', scenario.create);
 router.put('/scenario', scenario.update);
 router.delete('/scenario', scenario.delete);
 
 //Session Api
-router.get('/session', session.index);
+router.get('/sessions', session.index);
 router.get('/session/:id', session.show);
 router.post('/session', session.create);
 router.put('/session', session.update);
 router.delete('/session', session.delete);
 
 //User API
-router.get('/user', user.index);
+router.get('/users', user.index);
 router.get('/user/:id', user.show);
 router.post('/user', user.create);
 router.put('/user', user.update);
 router.delete('/user', user.delete);
 
 //Vote API
-router.get('/vote', vote.index);
+router.get('/votes', vote.index);
 router.get('/vote/:id', vote.show);
 router.post('/vote', vote.create);
 router.put('/vote', vote.update);
