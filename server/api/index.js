ea07fbfbf29a94b999066f05e12ee9ad77d5fcdc
cdr <cdr@iteratec.de>
2017-11-03 12:10:25 -0300

    fix delete api

--- server/api/index.js
+++ server/api/index.js
@@ -10,34 +10,34 @@ router.get('/decisions', decisions.index);
 router.get('/decision/:id', decisions.show);
 router.post('/decisions', decisions.create);
 router.put('/decision', decisions.update);
-router.delete('/decision', decisions.delete);
+router.delete('/decision/:id', decisions.delete);
 
 //scenario API
 router.get('/scenarios', scenario.index);
 router.get('/scenario/:id', scenario.show);
 router.post('/scenario', scenario.create);
 router.put('/scenario', scenario.update);
-router.delete('/scenario', scenario.delete);
+router.delete('/scenario/:id', scenario.delete);
 
 //Session Api
 router.get('/sessions', session.index);
 router.get('/session/:id', session.show);
 router.post('/session', session.create);
 router.put('/session', session.update);
-router.delete('/session', session.delete);
+router.delete('/session/:id', session.delete);
 
 //User API
 router.get('/users', user.index);
 router.get('/user/:id', user.show);
 router.post('/user', user.create);
 router.put('/user', user.update);
-router.delete('/user', user.delete);
+router.delete('/user/:id', user.delete);
 
 //Vote API
 router.get('/votes', vote.index);
 router.get('/vote/:id', vote.show);
 router.post('/vote', vote.create);
 router.put('/vote', vote.update);
-router.delete('/vote', vote.delete);
+router.delete('/vote/:id', vote.delete);
 
 module.exports = router;
